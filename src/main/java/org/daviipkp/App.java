54d00f8b2ffa594b8943bbfc3ae79bc7e8d65a7d
Daviiipkp <daviipkp@outlook.com>
2026-05-22 05:29:17 +0200

    initial commit

---
+++ src/main/java/org/daviipkp/App.java
@@ -0,0 +1,47 @@
+package org.daviipkp;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.daviipkp.App.CheckInRequest;
+
+import io.javalin.Javalin;
+
+public class App {
+
+    private static final Map<String, String> users = new ConcurrentHashMap<>();
+
+    public static void main( String[] args ) {
+        System.out.println("Initializing!");
+        System.out.println("Trying to initialize endpoints...");
+        initializeServer();
+        System.out.println("Done!");
+    }
+
+    public void initializeServer() {
+        Javalin j = Javalin.create(config -> {
+            config.bundledPlugins.enableCors(cors -> {
+                cors.addRule(rule -> rule.anyHost());
+            });
+            config.contextResolver.ip = ctx -> {
+                String forwardedFor = ctx.header("X-Forwarded-For");
+                return forwardedFor != null ? forwardedFor.split(",")[0] : ctx.req().getRemoteAddr();
+            };
+            config.routes.post("/checkin", ctx -> {
+                CheckInRequest req = ctx.bodyAsClass(CheckInRequest.class);
+                if(users.keySet().contains(ctx.ip())) {
+                    ctx.result("tu já fez checkin macho");
+                    return;
+                }
+                users.put(ctx.ip(), req.nick);
+                ctx.result("opa! check-in recebido :D");
+            });
+            
+        }).start(7070);
+    }
+
+    class CheckInRequest {
+        public String nick;
+    }
+
+}
